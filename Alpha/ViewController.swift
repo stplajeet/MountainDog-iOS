6950d40254132ce7150507d2f70a893e1cf9e05f
Monika Tiwari <monikatiwari@stpllko-pc121.local>
2018-03-28 11:05:13 +0530

    Initial Commit

---
+++ Alpha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alpha
+//
+//  Created by Monika Tiwari on 28/03/18.
+//  Copyright © 2018 Stpl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
